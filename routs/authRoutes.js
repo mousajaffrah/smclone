561f2aa89fa94ea3fc8f859dd79d841761d61f64
tala <tala.aunv@gmail.com>
2025-07-29 18:52:00 +0300

    authentication funcs

--- routs/authRoutes.js
+++ routs/authRoutes.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { RegisterUser, LoginUser } = require('../controller/authController');
 
-// Registration route
 router.post('/register', RegisterUser);
-
-// Login route
 router.post('/login', LoginUser);
 
 module.exports = router;
