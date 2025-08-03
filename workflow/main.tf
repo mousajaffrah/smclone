b2328f6010bb3a40b7ed1cc0a7a53a52ac02199b
noor halabi <halabinoor18@gmail.com>
2025-08-03 17:18:36 +0300

    deploy

---
+++ workflow/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c94855ba95c71c99" 
+  instance_type = "t2.micro"
+  key_name      = "noor"
+
+  tags = {
+    Name = "MyTerraformEC2"
+  }
+}
