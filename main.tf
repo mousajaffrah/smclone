ca0a9e347744ee3be700c5c9677c7c1773bc4034
noor halabi <halabinoor18@gmail.com>
2025-08-04 14:03:39 +0300

    deploynew

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t2.micro"
-  key_name      = "noor"
+  instance_type = "t3.micro"
+ 
 
   tags = {
     Name = "MyTerraformEC2"
