461b6c5fa146313029f4787190fee4a2e5819802
Noor Halabi <halabinoor18@gmail.com>
2025-08-04 13:54:43 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0c94855ba95c71c99"
   instance_type = "t3.micro"
-  key_name      = "Noor_Halabi"
+  key_name      = "noor"
 
   tags = {
     Name = "MyTerraformEC2"
