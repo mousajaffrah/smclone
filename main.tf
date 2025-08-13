bb9ed5233635b0b7af340ddebc1fc250875c9f60
Noor Halabi <halabinoor18@gmail.com>
2025-08-13 17:58:54 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t2.micro"
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 
+resource "aws_instance" "my_ec2" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
   associate_public_ip_address = true
 
   tags = {
